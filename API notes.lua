6d201901524c81de5e8ec58910b8d56123057880
lefixx <lefixx@gmail.com>
2022-08-11 20:54:31 +0300

    added flax and onion

--- API notes.lua
+++ API notes.lua
@@ -5,3 +5,6 @@ turtle.refuel(count)
 --number The maximum number of items to consume from the currently selected slot 
 
 turtle.refuel(0) --One can pass 0 to check if an item is combustable or not.
+
+North = positive Z
+East = positive X
