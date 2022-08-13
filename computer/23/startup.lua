34bf46c7df168ea3246dbcb1b156053ef07cb248
lefixx <lefixx@gmail.com>
2022-08-13 16:13:18 +0300

    Removed deleted drawer from furnace enging

--- computer/23/startup.lua
+++ computer/23/startup.lua
@@ -1,7 +1,7 @@
 furnace = peripheral.wrap("minecraft:furnace_2")
 drawer = peripheral.wrap("storagedrawers:standard_drawers_4_11")
 treeFarmDrawer = peripheral.wrap("storagedrawers:standard_drawers_4_15")
-turtleCoalDrawer = peripheral.wrap("storagedrawers:standard_drawers_1_26")
+-- turtleCoalDrawer = peripheral.wrap("storagedrawers:standard_drawers_1_26")
 
 charcoalSlot = 3
 logSlot = 2
@@ -15,7 +15,7 @@ while true do
     print(furnace.pullItems(peripheral.getName(drawer),charcoalSlot,1,topSlot)) --put charcoal in top slot
     print(furnace.pullItems(peripheral.getName(drawer),charcoalSlot,1,bottomSlot)) --put charcoal in bottom slot
     print(drawer.pullItems(peripheral.getName(furnace),outputSlot))
-    print(drawer.pushItems(peripheral.getName(turtleCoalDrawer),3))
+    -- print(drawer.pushItems(peripheral.getName(turtleCoalDrawer),3))
     print(furnace.pullItems(peripheral.getName(treeFarmDrawer),2,64,1)) --
     print(furnace.pullItems(peripheral.getName(drawer),4,64)) --
     -- print(drawer.pushItems(peripheral.getName(furnace),charcoalSlot,1,bottomSlot))
